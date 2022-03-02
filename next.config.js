88e359728ea3857275b91c3842c3bad52619797b
howmun <howmun.sitegiant@gmail.com>
2022-03-03 00:10:08 +0800

    try fix vercel config

--- next.config.js
+++ next.config.js
@@ -1,13 +1,13 @@
-/** @type {import('next').NextConfig} */
 const withPWA = require("next-pwa");
+const runtimeCaching = require("next-pwa/cache");
 
-const nextConfig = {
-  pwa: {
-    dest: "public",
-    register: true,
-    skipWaiting: true,
-    disable: process.env.NODE_ENV === 'development',
-  },
-}
-
-module.exports = withPWA(nextConfig);
+module.exports = withPWA({
+	reactStrictMode: true,
+	pwa: {
+		dest: "public",
+		register: true,
+		skipWaiting: true,
+		runtimeCaching,
+		buildExcludes: [/middleware-manifest.json$/]
+	}
+});
